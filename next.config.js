bb0e5808f1086a103945b9bee5d7d05c754860dc
jodypangaribuan <jodypangaribuan@gmail.com>
2026-02-21 02:22:44 +0700

    feat: Upgrade Next.js to v16, updating related dependencies and configuration files.

--- next.config.js
+++ next.config.js
@@ -24,9 +24,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   experimental: {
     optimizeCss: false,
     scrollRestoration: true,
